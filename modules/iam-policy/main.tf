01725b6ce11b1b24e354e882973e4f0eaf920d17
monsad <mon.sadlok@gmail.com>
2022-04-27 23:50:10 +0200

    add policy to module and create users

---
+++ modules/iam-policy/main.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_policy" "policy" {
+  count = var.create_policy ? 1 : 0
+
+  name        = var.name
+  path        = var.path
+  description = var.description
+
+  policy = data.aws_iam_policy_document.data_mining_access_s3.json
+
+  tags = var.tags
+}
+
+data "aws_iam_policy_document" "data_mining_access_s3" {
+
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketLocation",
+    ]
+    resources = ["arn:aws:s3:::var.bucket_name"]
+    effect    = "Allow"
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:RemoveObject",
+      "s3:GetObject",
+    ]
+    resources = ["arn:aws:s3:::var.bucket_name/*"]
+    effect = "Allow"
+  }
+}
