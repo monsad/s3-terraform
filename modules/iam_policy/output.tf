b2aaac5b8a02ea31d1155ac20c67cc41a154894a
monsad <mon.sadlok@gmail.com>
2022-05-01 16:29:58 +0200

    Update output.tf

--- modules/iam_policy/output.tf
+++ modules/iam_policy/output.tf
@@ -5,8 +5,4 @@ output "name" {
 output "arn" {
   description = "Assume role policy ARN of IAM group"
   value       = aws_iam_policy.policy.arn
-<<<<<<< HEAD:modules/iam_policy/output.tf
 }
-=======
-}
->>>>>>> d52a1c1d8bf29d511853b528ea9eba970342427f:modules/iam-policy/output.tf
